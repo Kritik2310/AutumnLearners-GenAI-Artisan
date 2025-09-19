a50c34ee9c3c58ea27d522b8a56be83ffef8f62a
Kritik2310 <kritikashyap2316@gmail.com>
2025-09-19 15:15:33 +0530

    .commit

--- artisan-upload/src/components/PhotosSection.jsx
+++ artisan-upload/src/components/PhotosSection.jsx
@@ -5,17 +5,24 @@ const PhotoSection = ({ images }) => {
   return (
     <section className="photo-section">
       <h1 className="gallery-title">Product Gallery</h1>
-      <div className="gallery-container">
-        {images.length > 0 ? (
+        <div className="gallery-container">
+            {images.length > 0 ? (
             images.map((image, index) => (
-            <div className="art-item" key={index}>
+                <div className="art-item" key={index}>
+                <img src={image} alt={`art piece ${index + 1}`} className="art-image" />
+                </div>
+            ))
+            ) : (
+            <p>No images uploaded yet. Please upload some of your artworks.</p>
+            )}
+
+            {/* Duplicate images to create a seamless loop */}
+            {images.map((image, index) => (
+            <div className="art-item" key={`duplicate-${index}`}>
                 <img src={image} alt={`art piece ${index + 1}`} className="art-image" />
             </div>
-            ))
-        ) : (
-            <p>no images uploaded yet. please upload some of your artworks</p>
-        )}
-      </div>
+            ))}
+        </div>
     </section>
   );
 };
